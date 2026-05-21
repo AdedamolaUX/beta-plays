9ffd6f98b3dbfc52da4b15cc32de082853d328a5
Adedamola Aregbesola <adedamolaux@gmail.com>
2026-05-21 01:24:11 +0100

    fix: custom wallet picker modal, fix disconnect crash, remove WalletModalProvider dep

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
-import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
 import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
 import '@solana/wallet-adapter-react-ui/styles.css'
 
@@ -18,9 +17,7 @@ function Root () {
   return (
     <ConnectionProvider endpoint={RPC_ENDPOINT}>
       <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
-          <App />
-        </WalletModalProvider>
+        <App />
       </WalletProvider>
     </ConnectionProvider>
   )
