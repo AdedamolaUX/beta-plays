1ebde42fe3ea87bd399f3ac2444ec62b56114409
Adedamola Aregbesola <adedamolaux@gmail.com>
2026-06-24 03:30:06 +0100

    iterating swap feature

--- src/App.jsx
+++ src/App.jsx
@@ -3809,7 +3809,6 @@ const openJupiterSwap = (token, walletContextState) => {
   Jupiter.init({
     displayMode: 'integrated',
     integratedTargetId: 'jupiter-plugin-inner',
-    endpoint: 'https://mainnet.helius-rpc.com/?api-key=a6f9ba84-1abf-4c90-8e04-fc0a61294407',
     enableWalletPassthrough: true,
     formProps: {
       initialInputMint: 'So11111111111111111111111111111111111111112',
